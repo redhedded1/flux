2517c1f9d4eced46ed5d70243c7b00ad238291fa
Larry Ball <larry@larryaball.net>
2015-12-09 15:28:00 -0600

    Initial Commit

--- excercises/5-flux/app/actions/ViewActionCreators.js
+++ excercises/5-flux/app/actions/ViewActionCreators.js
@@ -8,6 +8,12 @@ var ViewActionCreators = {
       type: ActionTypes.LOAD_CONTACTS
     });
     ApiUtil.loadContacts();
+  },
+  deleteUser (user) {
+    AppDispatcher.handleViewAction({
+      type: ActionTypes.DELETE_USER
+    });
+    ApiUtil.deleteUser(user);
   }
 };
 
--- excercises/5-flux/app/components/App.js
+++ excercises/5-flux/app/components/App.js
@@ -3,40 +3,46 @@ var ContactsStore = require('../stores/ContactsStore');
 var ViewActionCreators = require('../actions/ViewActionCreators');
 
 var App = React.createClass({
-  getInitialState () {
-    return ContactsStore.getState();
-  },
+    getInitialState () {
+        return ContactsStore.getState();
+    },
 
-  componentDidMount () {
-    ContactsStore.addChangeListener(this.handleStoreChange);
-    ViewActionCreators.loadContacts();
-  },
+    componentDidMount () {
+        ContactsStore.addChangeListener(this.handleStoreChange);
+        ViewActionCreators.loadContacts();
+    },
 
-  componentWillUnmount () {
-    ContactsStore.removeChangeListener(this.handleStoreChange);
-  },
+    componentWillUnmount () {
+        ContactsStore.removeChangeListener(this.handleStoreChange);
+    },
 
-  handleStoreChange () {
-    this.setState(ContactsStore.getState());
-  },
+    handleStoreChange () {
+        this.setState(ContactsStore.getState());
+    },
 
-  renderContacts () {
-    return this.state.contacts.map((contact) => {
-      return <li>{contact.first} {contact.last}</li>;
-    });
-  },
+    deleteUser (user){
+        ViewActionCreators.deleteUser(user);
+    },
 
-  render () {
-    if (!this.state.loaded) {
-      return <div>Loading...</div>;
+    renderContacts () {
+        return this.state.contacts.map((contact) => {
+            return <li>{contact.first} {contact.last}
+                <button onClick={this.deleteUser.bind(this,contact)}>delete</button>
+            </li>;
+        });
+    },
+
+    render () {
+        if (!this.state.loaded) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div>
+                <ul>{this.renderContacts()}</ul>
+            </div>
+        );
     }
-
-    return (
-      <div>
-        <ul>{this.renderContacts()}</ul>
-      </div>
-    );
-  }
 });
 
 module.exports = App;
